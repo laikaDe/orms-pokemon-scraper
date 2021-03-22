c1123326024b56f9ceda3a7791b28e96c3c83214
Laika Deunert <laikadeunert@Laikas-Laptop.local>
2021-03-21 19:49:04 -0700

    commit tree b7096bda4f170792a38b00afe1559e32274547b0

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,33 @@
 class Pokemon
+
+    attr_accessor :name, :type, :db
+    attr_reader :id
+
+    def initialize(id:, name:, type:, db:)
+        @id = id
+        @name = name 
+        @type = type
+        @db = db
+    end
+
+    def self.save(name, type, db)
+        
+        sql = <<-SQL
+        INSERT INTO pokemon (name, type)
+        VALUES (?, ?)
+        SQL
+  
+        db.execute(sql, name, type)
+        @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    end
+
+    def self.find(id, db)
+
+        # sql = "SELECT * FROM songs WHERE name = ?"
+        # result = DB[:conn].execute(sql, name)[0]
+        # Song.new(result[0], result[1], result[2])
+        sql = "SELECT * FROM pokemon WHERE id = ?"
+        result = db.execute(sql, id).flatten
+        Pokemon.new(id: result[0], name: result[1], type: result[2], db: db)
+    end
 end
